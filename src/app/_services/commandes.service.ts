b09a1ada0b800e0ffdad19bcfae0cc12f464cc73
Matthieuchv <81692038+Matthieuchv@users.noreply.github.com>
2022-03-08 19:50:16 +0100

    push

--- src/app/_services/commandes.service.ts
+++ src/app/_services/commandes.service.ts
@@ -1,12 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Order } from '../_models/order';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommandesService {
   
-
+private url =`${environment.apiUrl}/order`;
   
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAll():Observable<Order[]>{
+    return this.http.get<Order[]>(this.url);
+  }
+
+  getallOrderForUser(id:number):Observable<Order[]>{
+    return this.http.get<Order[]>(`${this.url}/findallforusertoday/${id}`);
+  }
 }
