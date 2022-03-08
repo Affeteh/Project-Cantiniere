4a93d6ed3a57c2010c77c717890a7afe28114d70
segolenedemolin <77448148+segolenedemolin@users.noreply.github.com>
2022-03-08 12:58:21 +0100

    register ok

--- src/app/_components/inscription/inscription.component.ts
+++ src/app/_components/inscription/inscription.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 import { UserIn } from 'src/app/_models/user-in';
 import { UserService } from 'src/app/_services/user.service';
 
@@ -9,10 +10,10 @@ import { UserService } from 'src/app/_services/user.service';
   styleUrls: ['./inscription.component.css']
 })
 export class InscriptionComponent implements OnInit {
-  
-  
-  @Input() user:UserIn= new UserIn(0,"","",0,"","","",false,"","","","",0,{def:false,image64:"",imagePath:"",id:0}); 
-  constructor(private userService:UserService) { }
+
+
+  @Input() user:UserIn= new UserIn(0,"","",0,"","","",false,"","","","",0,{def:false,image64:"",imagePath:"",id:0});
+  constructor(private userService:UserService, private router:Router) { }
 
   ngOnInit(): void {
   }
@@ -21,7 +22,8 @@ export class InscriptionComponent implements OnInit {
 
     this.userService.add(this.user).subscribe(result=>{
       this.user=result;
-      document.location.reload();
+      sessionStorage.setItem('userId',JSON.stringify(this.user.id));
+      this.router.navigateByUrl('');
     });
   }
 }
